8365aebc5ed0bdf32e5b4d00f0a9d6a2e8e59217
Anurag <anuragbanerjee103@gmail.com>
2025-06-27 12:47:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function goToMovies() {
   document.body.classList.add('opacity-0');
   setTimeout(() => {
-    window.location.href = '../for_movie/movie.html'; // Go up one level
+    window.location.href = 'movie.html'; // Go up one level
   }, 200);
 }
 
 function goToSeries() {
   document.body.classList.add('opacity-0');
   setTimeout(() => {
-    window.location.href = '../for_series/series.html';
+    window.location.href = 'series.html';
   }, 200);
 }
 
